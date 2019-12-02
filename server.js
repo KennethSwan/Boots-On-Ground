c55893e049d10f649e6f9ff5fa70d642f6b86b08
Ken Swan <Thekenswan@gmail.com>
2019-12-02 14:47:21 -0600

    server is connected

--- server.js
+++ server.js
@@ -4,6 +4,33 @@ const bodyParser = require('body-parser')
 const PORT = 4000; 
 
 
+app.get('/', (req, res) => {
+	res.send('Index')
+});
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+	dialect: 'sqlite', 
+	storage: "./database.sqlite3",
+	pool: {
+		max: 5, 
+		min: 0, 
+		acquire: 30000, 
+		idle: 10000
+	},
+});
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been successfully established.');
+	})
+	.catch(err => {
+		console.log('Test Connection: unable to connect to the database:', err);
+		if(err.original.code=='3D000') {
+			console.log('\n Did you forget paste the sql code in db/tables.sql?');
+		}
+	}); 
 
 
 
