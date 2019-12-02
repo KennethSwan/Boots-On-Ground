c56178cfaad1bf4877410998e2238d14072e48a1
Ken Swan <Thekenswan@gmail.com>
2019-12-02 16:37:19 -0600

    create models now in server.js

--- db/tables.sql
+++ db/tables.sql
@@ -6,26 +6,26 @@ CREATE DATABASE resource_db
 
 CREATE TABLE user (
 	id SERIAL PRIMARY KEY,
-	user_username VARCHAR(64),
+	username VARCHAR(64),
 	email VARCHAR(64),
 	password VARCHAR(64)
 );
 
 CREATE TABLE admin ( 
 	id SERIAL PRIMARY KEY,
-	admin_username VARCHAR(64),
+	adminusername VARCHAR(64),
 	password VARCHAR(64)
 ); 
 
 CREATE TABLE organization (
 	id SERIAL PRIMARY KEY,
-	organization_name VARCHAR(64),
+	organizationname VARCHAR(64),
 	password VARCHAR(64)
 ); 
 
 CREATE TABLE resource (
 	id SERIAL PRIMARY KEY,
-	organization_id INT REFERENCES organization(id) ON DELETE CASCADE,
+	organizationId INT REFERENCES organization(id) ON DELETE CASCADE,
 	description VARCHAR(64), 
 	url VARCHAR(64),
 	phone_number VARCHAR(64), 
--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const app = express();
 const bodyParser = require('body-parser')
 const PORT = 4000; 
 
-
-app.get('/', (req, res) => {
-	res.send('Index')
-});
-
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-	dialect: 'sqlite', 
-	storage: "./database.sqlite3",
+
+const sequelize = new Sequelize('resource_db', 'ken', null, {
+	host: 'localhost', 
+	dialect: 'postgres', 
+	operatorsAliases: false, 
+
 	pool: {
 		max: 5, 
 		min: 0, 
@@ -31,6 +29,41 @@ sequelize
 			console.log('\n Did you forget paste the sql code in db/tables.sql?');
 		}
 	}); 
+
+console.log("creating User model");
+const User = sequelize.define('user', {
+	username: Sequelize.STRING,
+	email: Sequelize.STRING, 
+	password: Sequelize.STRING
+})
+
+console.log("creating Admin model");
+const Admin = sequelize.define('admin', {
+	adminusername: Sequelize.STRING,
+	password: Sequelize.STRING
+})
+
+console.log("creating Organization model");
+const Organization = sequelize.define('organization', {
+	organizationname: Sequelize.STRING, 
+	password: Sequelize.STRING
+}) 
+
+console.log("creating Resource model");
+const Resource = sequelize.define('resource', {
+	description: Sequelize.STRING, 
+	url: Sequelize.STRING,
+	phonenumber: Sequelize.STRING,
+	category: Sequelize.STRING,
+	contactinfo: Sequelize.STRING
+}, {isUrl: true})
+
+Resource.belongsTo(Organization, {foreignKey: 'organizationId'})
+
+app.get('/', (req, res) => {
+	res.send('Index')
+});
+
 
 
 
