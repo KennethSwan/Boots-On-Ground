203b18efe777569ed62fe85c46a0224b733bb0b0
Ken Swan <Thekenswan@gmail.com>
2019-12-02 22:23:57 -0600

    mongoose is up and running

--- models/admin.js
+++ models/admin.js
@@ -1,1 +1,16 @@
-admin.js
+const mongoose = require('mongoose'); 
+
+const adminSchema = new mongoose.Schema({
+	username: {
+		type: String, 
+		required: true
+	},
+	password: {
+		type: String, 
+		required: true
+	}
+})
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin
--- server.js
+++ server.js
@@ -1,80 +1,26 @@
 const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser')
-const PORT = 4000; 
+const PORT = 4000;
+const methodOverride = require('method-override'); 
 
-
-// Config DB connection 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('resource_db', 'ken', null, {
-	host: 'localhost', 
-	dialect: 'postgres', 
-	operatorsAliases: false, 
-
-	pool: {
-		max: 5, 
-		min: 0, 
-		acquire: 30000, 
-		idle: 10000
-	},
-});
-
-// Connect to DB
-sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Connection has been successfully established.');
-	})
-	.catch(err => {
-		console.log('Test Connection: unable to connect to the database:', err);
-		if(err.original.code=='3D000') {
-			console.log('\n Did you forget paste the sql code in db/tables.sql?');
-		}
-	}); 
-
-// Create Models
-console.log("creating User model");
-const User = sequelize.define('user', {
-	username: Sequelize.STRING,
-	email: Sequelize.STRING, 
-	password: Sequelize.STRING
-})
-
-console.log("creating Admin model");
-const Admin = sequelize.define('admin', {
-	adminusername: Sequelize.STRING,
-	password: Sequelize.STRING
-})
-
-console.log("creating Organization model");
-const Organization = sequelize.define('organization', {
-	organizationname: Sequelize.STRING, 
-	password: Sequelize.STRING
-}) 
-
-console.log("creating Resource model");
-const Resource = sequelize.define('resource', {
-	description: Sequelize.STRING, 
-	url: Sequelize.STRING,
-	phonenumber: Sequelize.STRING,
-	category: Sequelize.STRING,
-	contactinfo: Sequelize.STRING
-}, {isUrl: true})
-
-Resource.belongsTo(Organization, {foreignKey: 'organizationId'})
+require('./db/db');
 
 // MIDDLEWARE 
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: false}))
+const adminController = require('./controllers/admin.js')
+app.use('/admin', adminController)
 
-app.get('/', (req, res) => {
-	res.send('Index')
-});
+const organizationController = require('./controllers/organization')
+app.use('/organization', organizationController)
 
+const resourceController = require('./controllers/resource')
+app.use('/resource', resourceController)
 
-
-
+const userController = require('./controllers/user.js')
+app.use('/user', userController)
 
 app.listen(PORT, () => {
 	console.log(`App is running on port ${PORT}`);
