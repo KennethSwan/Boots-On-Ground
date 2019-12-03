f9d40a5badd60a17db4c4773f263769900e8a9cc
Ken Swan <Thekenswan@gmail.com>
2019-12-03 13:56:16 -0600

    User login created

--- controllers/user.js
+++ controllers/user.js
@@ -28,4 +28,29 @@ router.post('/register', async(req, res, next) => {
 	}
 })
 
+// login route
+router.post('/login', async(req, res, next) => {
+	try {
+		const foundUser = await User.find({
+			username: req.body.username
+		})
+		if(foundUser.length === 0) {
+			return jsonify(data={}, status={"code": 401, "message": 'That username does not exists!'}), 401
+		}
+		else {
+			const password = req.body.password
+			if(bcrypt.compareSync(password, foundUser[0].password)){
+				req.session.user = foundUser[0]._id;
+				req.session.logged = true;	
+				return jsonsify(data={}, status={'code': 201, "message": "User successfully logged in"}), 200
+			}
+			else {
+				return jsonify(data={}, status={"code": 401, "message": 'Incorrect password!'}), 401
+			}
+		}
+	} catch(err) {
+		next(err)
+	}
+})
+
 module.exports = router;
