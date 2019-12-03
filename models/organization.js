f0b74a616f53f5c5bee219be41c6d6be644b4748
Ken Swan <Thekenswan@gmail.com>
2019-12-02 23:05:46 -0600

    Finished up models

--- models/organization.js
+++ models/organization.js
@@ -1,1 +1,15 @@
-organization.js
+const mongoose = require('mongoose'); 
+
+const organizationSchema = new.mongoose.Schema({
+	org_name: {
+		type: String, 
+		required: true
+	},
+	password: {
+		type: String, 
+		required: true
+	}
+})
+
+const Organization = mongoose.model('Organization', organizationSchema)
+module.exports = Organization
--- models/resource.js
+++ models/resource.js
@@ -1,1 +1,18 @@
-resource.js
+const mongoose = require('mongoose'); 
+
+const resourceSchema = new mongoose.Schema({
+	description: String, 
+	url: String, 
+	phone_number: String,
+	category: String,
+	contact_info: String,
+	organization: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Organization'
+	},
+})
+
+
+const Resource = mongoose.model('Resource', resourceSchema);
+
+module.exports = Resource
