d02b2997fe5da0af7b4ed20d252e0e37447dfd1f
Ken Swan <Thekenswan@gmail.com>
2019-12-04 11:33:09 -0600

    Organization can now login

--- controllers/organization.js
+++ controllers/organization.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Organization = require('../models/organization')
 const bcrypt = require('bcryptjs');
 
-
+// register/create org route 
 router.post('/register', async(req, res, next) => {
 	const org_name = req.body.org_name
 	console.log(org_name);
@@ -27,6 +27,31 @@ router.post('/register', async(req, res, next) => {
 		return res.status(201).json({"message": "Organization successfully created"})
 	} 
 	}catch(err){
+		next(err)
+	}
+})
+
+// organization login route
+router.post('/login', async(req, res, next) => {
+	try {
+		const foundOrg = await Organization.find({
+			org_name: req.body.org_name
+		})
+		if(foundOrg.length === 0) {
+			return res.status(401).json({"message": 'That organization does not exists!'})
+		}
+		else {
+			const password = req.body.password
+			if(bcrypt.compareSync(password, foundOrg[0].password)){
+				req.session.user = foundOrg[0]._id;
+				req.session.logged = true;	
+				return res.status(200).json({"message": "Organization successfully logged in"})
+			}
+			else {
+				return res.status(401).json({"message": 'Incorrect password!'})
+			}
+		}
+	} catch(err) {
 		next(err)
 	}
 })
